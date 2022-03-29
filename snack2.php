7c5d0c1bbda53fb3047da4e0d9d922783434911d
dvthnight <davide.lanotte@protonmail.com>
2022-03-29 15:52:16 +0200

    sostituito is_int con is_numeric

--- snack2.php
+++ snack2.php
@@ -2,10 +2,21 @@
 
     $nome = $_GET["nome"];
     $mail = $_GET["mail"];
-    $età = $_GET["età"];
+    $eta = $_GET["eta"];
 
-    if(isset($_GET["nome"])&& isset($_GET["mail"])&& isset($_GET["età"])){
-        if(strlen($nome)>3 && strpos( $mail,"@") && strpos( $mail, ".", -4) && is_int($età)){
+    echo "NOME: ";
+    echo (isset($_GET["nome"]) ? 'SI' : 'NO');
+
+    echo "<br>MAIL: ";
+    echo (isset($_GET["mail"]) ? 'SI' : 'NO');
+
+    echo "<br>ETA: ";
+    echo (isset($_GET["eta"]) ? 'SI' : 'NO');
+    
+    echo "<br>";
+
+    if(isset($_GET["nome"])&& isset($_GET["mail"])&& isset($_GET["eta"])){
+        if(strlen($nome)>3 && strpos( $mail,"@") && strpos( $mail, ".", -4) && is_numeric($eta)){
             echo "Acesso riuscito";
         } else {
             echo "Accesso negato";
